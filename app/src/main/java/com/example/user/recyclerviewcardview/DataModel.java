c4de1db18184ac5a0281f30d6c2ce8a3baf90b4b
anju-abraham <46350051+anju-abraham@users.noreply.github.com>
2019-02-24 15:50:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/recyclerviewcardview/DataModel.java
@@ -0,0 +1,36 @@
+package com.example.user.recyclerviewcardview;
+
+/**
+ * Created by user on 24-Feb-19.
+ */
+////The DataModel is used to retrieve the data for each CardView through getters
+public class DataModel {
+
+    String name;
+    String version;
+    int id_;
+    int image;
+
+    public DataModel(String name, String version, int id_, int image) {
+        this.name = name;
+        this.version = version;
+        this.id_ = id_;
+        this.image=image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public int getId() {
+        return id_;
+    }
+}
